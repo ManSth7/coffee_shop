d65b7871700cd0f4318a0caa829eedf68741fe75
Ubuntu <ubuntu@ip-172-31-40-216.us-east-2.compute.internal>
2019-07-31 07:10:24 +0000

    test

--- grunt/config/connect.js
+++ grunt/config/connect.js
@@ -1,7 +1,7 @@
 module.exports = function (grunt, options) {
 
   var port = grunt.option('port') || 3000;
-  var host = grunt.option('host') || "elearning-course.com";
+  var host = grunt.option('host') || "18.224.181.145";
 
   return {
     server: {
